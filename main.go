722f082a61b49f8a303406af3a3f260b7452cad8
pauljako <paulifey18@proton.me>
2024-11-02 18:50:10 +0100

    feat(main): added support for going back

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"strings"
 )
 
 type Json struct {
@@ -19,15 +20,15 @@ type Json struct {
 }
 
 func main() {
-	var startUrl string = "https://kleefuchs.github.io/OS-Picker/assets"
+	var startUrl []string = []string{"https://kleefuchs.github.io/OS-Picker/assets"}
 	err := initialize(startUrl)
 	if err != nil {
 		log.Fatalln(err)
 	}
 }
 
-func initialize(url string) error {
-	json, err := getJson(url + "/index.json")
+func initialize(url []string) error {
+	json, err := getJson(strings.Join(append(url, "index.json"), "/"))
 	if err != nil {
 		return err
 	}
@@ -37,23 +38,29 @@ func initialize(url string) error {
 	if json.Description != "" {
 		fmt.Println("\n" + json.Description)
 	}
+	if len(url) > 1 {
+		fmt.Printf("\n%v) %v", 0, "Back")
+	}
 	if len(json.Buttons) > 0 {
 		for index, element := range json.Buttons {
 			fmt.Printf("\n%v) %v", index + 1, element.Text)
 		}
-		fmt.Print("\n\n")
-		var input uint
-		fmt.Print("> ")
-		fmt.Scanln(&input)
-		if int(input) > len(json.Buttons) || input == 0{
-			err = fmt.Errorf("invalid value: %v", input)
-			return err
-		}
-		var newurl string = url + "/" + json.Buttons[input - 1].NextPath
-		return initialize(newurl)
-	} else {
-		return nil
 	}
+	fmt.Print("\n\n")
+	var input uint
+	fmt.Print("> ")
+	fmt.Scanln(&input)
+	if int(input) == 0 {
+		var newurl []string = url[:len(url)-1]
+		return initialize(newurl)
+	} else if int(input) > len(json.Buttons) {
+		err = fmt.Errorf("invalid value: %v", input)
+		return err
+	} else {
+		var newurl []string = append(url, json.Buttons[input - 1].NextPath)
+		return initialize(newurl)
+	}
+	return nil
 }
 
 func getJson(url string) (Json, error) {
